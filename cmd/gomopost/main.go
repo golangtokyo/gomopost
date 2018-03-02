24dc9b8529933ba76643a52a1c54ead02e3e07c1
pankona <yosuke.akatsuka@gmail.com>
2018-03-02 14:26:10 +0900

    add main function

---
+++ cmd/gomopost/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/pankona/gomopost"
+	"github.com/urfave/cli"
+)
+
+// GlobalFlags mean flags specified by command line
+var GlobalFlags = []cli.Flag{
+	cli.StringFlag{
+		Name:  "a, address",
+		Value: "http://localhost:8080",
+		Usage: "specify server address (scheme and port required)",
+	},
+	cli.StringFlag{
+		Name:  "n, name",
+		Value: "anonymous",
+		Usage: "specify name",
+	},
+	cli.StringFlag{
+		Name:  "m, message",
+		Value: "hello",
+		Usage: "specify message",
+	},
+}
+
+func defaultCommand(c *cli.Context) {
+	cli := gomopost.NewClient("http://localhost:8080")
+	err := cli.Post("pankona", "test message from pankona")
+	if err != nil {
+		fmt.Printf("failed to post: %s\n", err.Error())
+	}
+}
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "gomopost"
+	app.Usage = "simple http client for gomobile instruction"
+	app.Version = "0.1"
+	app.Flags = GlobalFlags
+	app.Action = defaultCommand
+
+	cli.AppHelpTemplate = `NAME:
+   {{.Name}} - {{.Usage}}
+
+USAGE:
+   {{.Name}} [options] [arguments...]
+
+VERSION:
+   {{.Version}}{{if or .Author .Email}}
+
+AUTHOR:{{if .Author}}
+  {{.Author}}{{if .Email}} - <{{.Email}}>{{end}}{{else}}
+  {{.Email}}{{end}}{{end}}
+
+OPTIONS:
+   {{range .Flags}}{{.}}
+   {{end}}
+`
+
+	err := app.Run(os.Args)
+	if err != nil {
+		os.Exit(1)
+	}
+	os.Exit(0)
+}
